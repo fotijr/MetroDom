bf996663aee45ee0ac7ccbc5dee045d3cf5083e5
Dominic <dominic@fotijr.com>
2016-09-11 20:55:53 -0400

    Added first xunit tests and updating CI to run tests

---
+++ MetroDom.Core/Orchestra.cs
@@ -0,0 +1,32 @@
+﻿using MetroDom.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroDom.Core
+{
+    public class Orchestra
+    {
+        private ICollection<IInstrument> _instruments;
+
+        public Orchestra(ICollection<IInstrument> instruments)
+        {
+            _instruments = instruments;
+        }
+
+        /// <summary>
+        /// Start playing song with all instruments.
+        /// </summary>
+        /// <param name="song"></param>
+        public void Start(Song song)
+        {
+            if (song == null) throw new ArgumentNullException("No song provided.");
+            if (_instruments == null || _instruments.Count == 0) return;
+
+            // TODO: actually play song here
+
+        }
+    }
+}
---
+++ MetroDom.Tests/Core/OrchestraTests.cs
@@ -0,0 +1,38 @@
+﻿using MetroDom.Core;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MetroDom.Tests.Core
+{
+    public class OrchestraTests
+    {
+        [Fact]
+        public void Start_IsSuccessful_WithNullInstruments()
+        {
+            // Arrange
+            var orchestra = new Orchestra(null);
+            var song = new Song(new List<SongNote> { new SongNote(Note.C, 500) });
+
+            // Act
+            orchestra.Start(song);
+
+            // Assert
+        }
+
+        [Fact]
+        public void Start_ThrowsArgumentNullException_WithNullSong()
+        {
+            // Arrange
+            var orchestra = new Orchestra(null);
+
+            // Act        
+            var exception = Record.Exception(() => orchestra.Start(null));
+
+            // Assert
+            Assert.NotNull(exception);
+            Assert.IsType<ArgumentNullException>(exception);
+        }
+
+    }
+}
